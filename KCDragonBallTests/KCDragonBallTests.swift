bdf54a2e6516238dde65acb631c5de09b83ed349
Quinterock <luisgqr1975@gmail.com>
2025-03-14 12:09:07 -0600

    Parte opcional terminada con testeo

--- KCDragonBallTests/KCDragonBallTests.swift
+++ KCDragonBallTests/KCDragonBallTests.swift
@@ -10,6 +10,7 @@ import XCTest
 
 final class HeroTests: XCTestCase {
     private var sut: Hero!
+    private var sutTransform: Transformation!
     
     func test_init_withName_isGoku() {
         // Given/ Arrange
@@ -26,5 +27,22 @@ final class HeroTests: XCTestCase {
         
         // Then/ Assert
         XCTAssertEqual(sut.name, name)
+    }
+    
+    // Comprobar que existe una transformacion con el nombre de Kaio-Ken
+    func test_Transformation_withName_kaioken_exist() {
+        // Given / Arrange
+        let transformation = "Kaio-Ken"
+        
+        // When / Act
+        sutTransform = Transformation(
+            name: transformation,
+            id: "x",
+            photo: "x",
+            description: "x"
+        )
+        
+        // Then / Assert
+        XCTAssertEqual(sutTransform.name, transformation)
     }
 }
--- KCDragonBallTests/NetworkModelTests.swift
+++ KCDragonBallTests/NetworkModelTests.swift
@@ -33,10 +33,35 @@ final class NetworkModelTests: XCTestCase {
         // Then
         XCTAssertEqual(successResult, receivedResult)
         XCTAssertEqual(
-            apiClient.receivedRequest?.url, 
+            apiClient.receivedRequest?.url,
             URL(string: "https://dragonball.keepcoding.education/api/heros/all")
         )
         let authorizationHeaderValue = apiClient.receivedRequest?.allHTTPHeaderFields?["Authorization"]
         XCTAssertEqual(authorizationHeaderValue, "Bearer some-token")
+    }
+    
+    func test_login_expectedToken_succes() {
+        // Given
+        let user = "testUser"
+        let password = "pass"
+        let expectedTokenMock = "token"
+        
+        
+        //Expected -  Aqui simulamos que la respuesta delm server es exitosa,
+        // .succes y que el token que devuelve es el token que hicimos (expectedTokenMock)
+        apiClient.receivedTokenResult = .success(expectedTokenMock)
+        
+        // When
+        var resultToken: String?
+        sut.login(user: user, password: password) { result in
+            if case .success(let token) = result {
+                resultToken = token
+            }
+            
+        }
+        
+        // Then
+        // El token recibido sea igual al expectedTokenMock y si lo son, el login es exitoso
+        XCTAssertEqual(resultToken, expectedTokenMock)
     }
 }
